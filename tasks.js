ae0013c745f249a657ef159de3bba23c21625ffe
spogmaiMayar <spogmaimayar1@gmail.com>
2023-04-09 14:53:38 +0430

    push

---
+++ tasks.js
@@ -0,0 +1,32 @@
+var country = 'Afghanistan'
+var continent = 'Asia'
+var population = 40;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//var country = "afghanistan"
+
+
+var isIsland = true
+var language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+var language = "Dari"
+const native_language = "Pashto"
+
+
+var population = 40;
+console.log(population);
+console.log(population/2);
+var finland_population = 6;
+console.log(population > 6);
+var average_population = 33;
+console.log(population > average_population);
+var description = `${country} is in ${continent}. Its populaion is ${population} million people, speaking ${language} and ${native_language} languages. `
+console.log(description);
